7175b7124070459d3b30b154478d2cbbb17b45f3
Eric Downs <eric@grainandmortar.com>
2025-07-16 13:49:23 -0500

    Update site

--- contact.php
+++ contact.php
@@ -5,7 +5,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Westervelt Real Estate</title>
+	<title>Westervelt Real Estate - Contact</title>
 	<link rel="stylesheet" href="style.css">
 </head>
 <body class="min-h-screen flex flex-col">
