15bc0b307bf87cce7c0404662e7ec03d0c7df948
Eric Downs <eric@grainandmortar.com>
2025-07-17 13:31:15 -0500

    Update site

---
+++ land-for-sale-image-expanded.php
@@ -0,0 +1,29 @@
+<!-- <style> *{outline: 1px solid rgba(0, 0, 0, .5);} </style> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Westervelt Real Estate</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body class="min-h-screen flex flex-col">
+	<?php include './includes/header.php'; ?>
+
+
+	
+
+	<main class="mx-auto flex-grow w-full">
+
+		
+		<h1>Sorry Page Not Found</h1>
+
+
+
+	</main>
+
+
+	<?php include './includes/footer.php'; ?>
+</body>
+</html>
