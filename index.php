da7258ec8d82cb0a3c0401d82a4099526e22771f
Eric Downs <eric@grainandmortar.com>
2025-07-23 14:23:49 -0500

    Update site

--- index.php
+++ index.php
@@ -101,7 +101,46 @@
 
 		<div>
 			<div class="grid md:grid-cols-2 grid-cols-1 gap-y-10 gap-x-5 grid_cards_land_wrapper px-5 py-20">
-				<?php for ($i = 0; $i < 2; $i++) {include './components/cards/card_buying.php'; } ?>
+				<a href="#" class="block group hover:shadow-lg transition-shadow easy rounded-xl overflow-clip">
+					<div class="flex flex-col bg-white card_land">
+						<div class="relative pb-[95%] w-full">
+							<img
+							class="absolute h-full w-full object-cover transform group-hover:scale-[1.045] easy"
+							src="/assets/images/card-buying.jpg"
+							alt="Westervelt buying services property image"
+							>
+							
+							<div class="absolute bottom-0 h-2/3 w-full bg-gradient-to-t from-black to-black/0 pointer-events-none"></div>
+
+							<div class="flex flex-col justify-end items-start space-y-4 p-6 rounded absolute h-full w-full *:text-white">
+								<span class="badge">We can Help</span>
+								<h2>Buying</h2>
+								<h5>Our skilled Westervelt Real Estate team is here for you every step of the way. Check out our listings.</h5>
+							</div>
+
+						</div>
+					</div>
+				</a>
+				<a href="#" class="block group hover:shadow-lg transition-shadow easy rounded-xl overflow-clip">
+					<div class="flex flex-col bg-white card_land">
+						<div class="relative pb-[95%] w-full">
+							<img
+							class="absolute h-full w-full object-cover transform group-hover:scale-[1.045] easy"
+							src="/assets/images/selling.jpg"
+							alt="Westervelt selling services property image"
+							>
+							
+							<div class="absolute bottom-0 h-2/3 w-full bg-gradient-to-t from-black to-black/0 pointer-events-none"></div>
+
+							<div class="flex flex-col justify-end items-start space-y-4 p-6 rounded absolute h-full w-full *:text-white">
+								<span class="badge">We're Always Looking</span>
+								<h2>Selling</h2>
+								<h5>Do you have land you're interested in selling? Contact us and tell us more!</h5>
+							</div>
+
+						</div>
+					</div>
+				</a>
 			</div>
 		</div>
 
